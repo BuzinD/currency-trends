1675730d14260b1bc406973e5936d400758ebac2
d.buzin <d.buzin@fairtech.ru>
2025-01-29 19:05:35 +0300

    Added plan running fetch currencies and candles

--- app/internal/app/app.go
+++ app/internal/app/app.go
@@ -2,29 +2,56 @@ package app
 
 import (
 	"cur/internal/config"
+	"cur/internal/infrastructure/dbConnection"
+	"cur/internal/service/okx"
+	"cur/internal/store"
 	"os"
 
+	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
 )
 
 type App struct {
 	config *config.Config
 	log    *log.Logger
+	store  *store.Store
 }
 
 func (app *App) initLogger() {
+	app.log = log.New()
 	app.log.SetFormatter(&log.JSONFormatter{})
 	app.log.SetOutput(os.Stdout)
 }
 
-var app *App
-
 func StartApplication() error {
-	app = newApp()
+	app := newApp()
 	app.initConfig()
 	app.initLogger()
+	app.initStore()
 
-	return nil
+	okxService := okx.NewOkxService(app.store.Currency()) //TODO set config during creation
+
+	c := cron.New()
+
+	// Running at 9:00 и 21:00 every day
+	c.AddFunc("0 8,21 * * *", func() {
+		if err := okxService.UpdateCurrencies(app.Config().OkxApiConfig()); err != nil {
+			app.log.Error(err)
+		}
+	})
+
+	// Running at 8:00 every day
+	c.AddFunc("0 9 * * *", func() {
+		if err := okx.UpdateTickers(app.Config()); err != nil {
+			app.log.Error(err)
+		}
+	})
+
+	// running
+	c.Start()
+
+	// Waiting
+	select {}
 }
 
 func newApp() *App {
@@ -37,6 +64,20 @@ func (app *App) initConfig() error {
 	return nil                      //todo
 }
 
+func (app *App) initStore() error {
+	db, err := dbConnection.GetDbConnection()
+	if err != nil {
+		return err
+	}
+
+	app.store = store.NewStore(db) // Initialize configuration
+	return nil
+}
+
 func (app *App) Log() *log.Logger {
 	return app.log
+}
+
+func (app *App) Config() *config.Config {
+	return app.config
 }
