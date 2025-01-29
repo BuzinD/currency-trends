48e316a70a2d917ce00fb8ee1bb632562a669707
d.buzin <d.buzin@fairtech.ru>
2025-01-29 18:43:14 +0300

    fix currencies table structure

---
+++ app/migrations/20250127143626_CreateCurrencyCodeTable.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE currencies (
+    id SERIAL PRIMARY KEY,               -- Unique id
+    code VARCHAR(10) NOT NULL,           -- Code (BTC, ETH)
+    chain VARCHAR(100) NOT NULL,         -- Network name USDT-TRC20, USDT-ERC20 etc.
+    can_deposit BOOLEAN NOT NULL,        -- Can Deposit
+    can_withdraw BOOLEAN NOT NULL,        -- Can Withdraw
+    UNIQUE (code, chain)
+);
+
+-- Idx for finding by code
+CREATE INDEX idx_currency_code ON currencies (code);
