f521b8e218dc0fc07696239d8e8e7051687be4b8
d.buzin <d.buzin@fairtech.ru>
2025-01-29 21:58:58 +0300

    Move scheduled process into separated function

--- app/internal/app/app.go
+++ app/internal/app/app.go
@@ -15,6 +15,7 @@ type App struct {
 	config *config.Config
 	log    *log.Logger
 	store  *store.Store
+	cron   *cron.Cron
 }
 
 func (app *App) initLogger() {
@@ -28,30 +29,9 @@ func StartApplication() error {
 	app.initConfig()
 	app.initLogger()
 	app.initStore()
+	app.initScheduledTasks()
 
-	okxService := okx.NewOkxService(app.store.Currency()) //TODO set config during creation
-
-	c := cron.New()
-
-	// Running at 9:00 и 21:00 every day
-	c.AddFunc("0 8,21 * * *", func() {
-		if err := okxService.UpdateCurrencies(app.Config().OkxApiConfig()); err != nil {
-			app.log.Error(err)
-		}
-	})
-
-	// Running at 8:00 every day
-	c.AddFunc("0 9 * * *", func() {
-		if err := okx.UpdateTickers(app.Config()); err != nil {
-			app.log.Error(err)
-		}
-	})
-
-	// running
-	c.Start()
-
-	// Waiting
-	select {}
+	return nil
 }
 
 func newApp() *App {
@@ -80,4 +60,33 @@ func (app *App) Log() *log.Logger {
 
 func (app *App) Config() *config.Config {
 	return app.config
+}
+
+func (app *App) initScheduledTasks() {
+	okxService := okx.NewOkxService(app.store.Currency()) //TODO set config during creation
+	app.cron = cron.New()
+
+	// Running at 8:00 и 21:00 every day
+	app.cron.AddFunc("* 8,21 * * *", func() {
+		app.log.Info("process update currencies started")
+		if err := okxService.UpdateCurrencies(app.Config().OkxApiConfig()); err != nil {
+			app.log.Error(err)
+		}
+		app.log.Info("process update currencies finished")
+	})
+
+	// Running at 9:00 every day
+	app.cron.AddFunc("0 9 * * *", func() {
+		app.log.Info("process update tickers started")
+		if err := okx.UpdateTickers(app.Config()); err != nil {
+			app.log.Error(err)
+		}
+		app.log.Info("process update tickers finished")
+	})
+
+	// running
+	app.cron.Start()
+
+	// Waiting
+	select {}
 }
