7e2143312679ea5a0dea2894791357af622c30e0
BuzinD <dmitrybuzin@gmail.com>
2025-01-29 18:46:59 +0300

    Added currency response data structure

---
+++ app/internal/structure/response/currenciesResponse.go
@@ -0,0 +1,14 @@
+package structure
+
+type CurrencyResponse struct {
+	Code string                 `json:"code"`
+	Msg  string                 `json:"msg"`
+	Data []CurrencyResponseData `json:"data"`
+}
+
+type CurrencyResponseData struct {
+	Ccy    string `json:"ccy"`
+	Chain  string `json:"chain"`
+	CanDep bool   `json:"canDep"`
+	CanWd  bool   `json:"canWd"`
+}
