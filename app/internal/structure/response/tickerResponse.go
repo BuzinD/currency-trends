9bc9d918a5c0e4afa8adc596c2c9385d684fe13b
BuzinD <dmitrybuzin@gmail.com>
2025-01-29 18:51:18 +0300

    Added tickerResponse data tructure

--- app/internal/structure/response/tickerResponse.go
+++ app/internal/structure/response/tickerResponse.go
@@ -1,15 +1,17 @@
 package structure
 
 type TickerResponse struct {
-	Code string `json:"code"`
-	Msg  string `json:"msg"`
-	Data []struct {
-		InstId    string `json:"instId"`
-		Last      string `json:"last"`
-		Open24H   string `json:"open24h"`
-		High24H   string `json:"high24h"`
-		Low24H    string `json:"low24h"`
-		Vol24H    string `json:"vol24h"`
-		Timestamp string `json:"ts"`
-	} `json:"data"`
+	Code string               `json:"code"`
+	Msg  string               `json:"msg"`
+	Data []TickerResponseData `json:"data"`
+}
+
+type TickerResponseData struct {
+	InstId    string `json:"instId"`
+	Last      string `json:"last"`
+	Open24H   string `json:"open24h"`
+	High24H   string `json:"high24h"`
+	Low24H    string `json:"low24h"`
+	Vol24H    string `json:"vol24h"`
+	Timestamp string `json:"ts"`
 }
