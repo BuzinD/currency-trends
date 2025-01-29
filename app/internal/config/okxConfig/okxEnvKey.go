b8f8e705df423842175381962c5c0dd731776ab4
BuzinD <dmitrybuzin@gmail.com>
2025-01-29 18:46:02 +0300

    update okxApiConfig

--- app/internal/config/okxConfig/okxEnvKey.go
+++ app/internal/config/okxConfig/okxEnvKey.go
@@ -3,7 +3,11 @@ package okxConfig
 type OkxEnvKey string
 
 const (
-	ApiKey     = "API_KEY"
-	Secret     = "SECRET"
-	PassPhrase = "PASSPHRASE"
+	ApiKey         = "API_KEY"
+	Secret         = "SECRET"
+	PassPhrase     = "PASSPHRASE"
+	ApiUri         = "API_URI"
+	CandlesPath    = "CANDLES_PATH"
+	TickersPath    = "TICKERS_PATH"
+	CurrenciesPath = "CURRENCIES_PATH"
 )
